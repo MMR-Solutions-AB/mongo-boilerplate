d71006130e45e583d297cf1852471e74b025e2a1
Matt Bergstrom <matt@mmrsolutions.se>
2021-12-13 12:13:54 +0100

    klarna integration with webshop complete

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -1,12 +1,16 @@
 const app = require('../../loaders/express-handlebars');
 
 const { createOrder } = require('../../services/server/klarna');
+const { getFakeStoreCart } = require('../../services/server/fakestore');
 
 app.get('/checkout/:cart_id', async function (req, res, next) {
 	// Replace with HTML snippet from CreateOrder Klarna Request
 	const cart_id = req.params.cart_id;
+	const cartItems = JSON.parse(req.query.cartItems);
 
-	const klarnaJsonResponse = await createOrder(cart_id);
+	const completeCart = await getFakeStoreCart(cartItems);
+
+	const klarnaJsonResponse = await createOrder(completeCart);
 
 	const html_snippet = klarnaJsonResponse.html_snippet;
 
